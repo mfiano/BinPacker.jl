bb8e3dee18ce1a5aeb465d3c576f3c76a7dc48a9
Documenter.jl <documenter@juliadocs.github.io>
2022-09-25 18:15:29 +0000

    build based on ca68428

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BinPacker","category":"page"},{"location":"#BinPacker","page":"Home","title":"BinPacker","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BinPacker.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BinPacker]","category":"page"}]
+}
