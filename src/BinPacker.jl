d689330f92aef7560bb7de3df95ecd35adb4de8f
Michael Fiano <mail@mfiano.net>
2022-10-04 13:06:35 -0400

    refactor: move bin options to separate file

--- src/BinPacker.jl
+++ src/BinPacker.jl
@@ -7,6 +7,7 @@ export Bin, Packer, Rect, pack
 include("algorithm_types.jl")
 include("rect.jl")
 include("compare.jl")
+include("bin_options.jl")
 include("bin.jl")
 include("packer.jl")
 
--- src/bin.jl
+++ src/bin.jl
@@ -1,32 +1,3 @@
-struct BinOptions{F <: FitnessAlgorithm}
-    max_size::NTuple{2, Int}
-    padding::Int
-    border::Int
-    rotate::Bool
-    resize_by::NTuple{2, Int}
-    pow2::Bool
-    square::Bool
-    fit_by::F
-end
-
-function BinOptions(max_size, padding, border, rotate, resize_by, pow2, square, fit_by::Symbol)
-    fit_by = fitness_algorithm_value(Val(fit_by))
-    BinOptions(max_size, padding, border, rotate, resize_by, pow2, square, fit_by)
-end
-
-function BinOptions(;
-    max_size=(4096, 4096),
-    padding=0,
-    border=0,
-    rotate=false,
-    resize_by=(1, 1),
-    pow2=false,
-    square=false,
-    fit_by=:area
-)
-    BinOptions(max_size, padding, border, rotate, resize_by, pow2, square, fit_by)
-end
-
 mutable struct Bin{F <: FitnessAlgorithm}
     width::Int
     height::Int
